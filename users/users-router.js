1132d774a4b4646861fa3c95034c0407bd02a325
dislersd <dislersd@gmail.com>
2019-04-01 14:06:30 -0700

    removed get user route - only adding users through register route

--- users/users-router.js
+++ users/users-router.js
@@ -11,15 +11,6 @@ router.get('/', async (req, res) => {
   }
 })
 
-router.post('/', async (req, res) => {
-  try {
-    await db.add(req.body);
-    res.status(201).json({ message: 'success!' })
-  } catch (error) {
-    res.status(500).json({ message: 'error creating user' })
-  }
-})
-
 module.exports = router;
 
 
