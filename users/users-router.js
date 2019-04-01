607ae709f2af1d0cd1b1785e2e02fa3d4a8a0346
dislersd <dislersd@gmail.com>
2019-04-01 13:41:06 -0700

    migrated users table to db

--- users/users-router.js
+++ users/users-router.js
@@ -2,6 +2,15 @@ const router = require('express').Router();
 
 const db = require('./users-model.js')
 
+router.get('/', async (req, res) => {
+  try {
+    const users = await db.find();
+    res.status(200).json(users)
+  } catch (error) {
+    res.status(500).json({ message: 'error finding users' })
+  }
+})
+
 module.exports = router;
 
 
