bf562c1651f2f1b85b332972b59bddd2fddd779e
dislersd <dislersd@gmail.com>
2019-04-02 11:45:57 -0700

    yarn added express-session

--- users/users-router.js
+++ users/users-router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Users = require("./users-model.js");
 const bcrypt = require("bcryptjs");
 
-router.get("/",  restricted, async (req, res)  => {
+router.get("/", restricted, async (req, res) => {
   try {
     const users = await Users.find();
     res.status(200).json(users);
@@ -11,41 +11,43 @@ router.get("/",  restricted, async (req, res)  => {
   }
 });
 
-// async function restricted(req, res, next) {
-//   const { username, password } = req.headers;
-//   try {
-//     if (username && password) {
-//       const user = await Users.findBy({ username }).first();
-//       if (user && bcrypt.compareSync(password, user.password)) {
-//         next();
-//       } else {
-//         res.status(401).json({ message: "You shall not pass!" });
-//       }
-//     }
-//   } catch (error) {
-//     res.status(500).json({ message: "please provide credentials" });
-//   }
-// }
-
-function restricted(req, res, next) {
+async function restricted(req, res, next) {
   const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: "You cannot pass!" });
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
-  } else {
-    res.status(401).json({ message: "please provide credentials" });
+  try {
+    if (username && password) {
+      const user = await Users.findBy({ username }).first();
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    } else {
+      res.status(406).json({ message: "please provide credentials" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "please provide credentials" });
   }
 }
+
+// function restricted(req, res, next) {
+//   const { username, password } = req.headers;
+
+//   if (username && password) {
+//     Users.findBy({ username })
+//       .first()
+//       .then(user => {
+//         if (user && bcrypt.compareSync(password, user.password)) {
+//           next();
+//         } else {
+//           res.status(401).json({ message: "You cannot pass!" });
+//         }
+//       })
+//       .catch(error => {
+//         res.status(500).json(error);
+//       });
+//   } else {
+//     res.status(401).json({ message: "please provide credentials" });
+//   }
+// }
 
 module.exports = router;
