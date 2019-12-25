e5476184e86ce8381244b00a098eba9ddead43f6
dislersd <dislersd@gmail.com>
2019-12-24 20:31:32 -0800

    update

--- gatsby-front-end/src/services/auth.js
+++ gatsby-front-end/src/services/auth.js
@@ -10,20 +10,19 @@ export const getUser = () =>
 const setUser = user =>
   window.sessionStorage.setItem("user", JSON.stringify(user))
 
-export const handleLogin = (state) => {
-    console.log('Hello')
-    axios.post("http://localhost:3000/api/login", state)
+export const handleLogin = state => {
+  axios
+    .post("http://localhost:3000/api/login", state)
     .then(res => setUser(res.data.cookie.username))
     .catch(err => console.log(err))
 }
 
 export const isLoggedIn = () => {
-    if (window.sessionStorage.getItem("user")) {
-        console.log();
-    }
+  const user = getUser()
+  return !!user.username
 }
 
-// export const logout = callback => {
-//   setUser({})
-//   callback()
-// }
+export const logout = callback => {
+  setUser({})
+  callback()
+}
